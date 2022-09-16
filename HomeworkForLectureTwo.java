1dac63c22e7292999b481318a18abe6d569fc8d9
M.S.Shevchenko <m.s.shevchenko@list.ru>
2022-09-16 16:45:20 +0400

    release v1_haha

--- HomeworkForLectureTwo.java
+++ HomeworkForLectureTwo.java
@@ -1,5 +1,5 @@
 public class HomeworkForLectureTwo {
     public static void main(String[] args) {
-        System.out.println("Here is the homework for lecture 2");
+        System.out.println("Here is the homework for lecture 2 and learning Git");
     }
 }
